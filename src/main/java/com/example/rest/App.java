54205f836f0e61cb413e46188da7525636976c2f
EddyOnGitHub <40594125+EddyOnGitHub@users.noreply.github.com>
2018-06-26 11:12:31 +0200

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -43,7 +43,7 @@ public class App {
  
         System.out.println("Server started");
  
-        //runGetRequest();
+        runGetRequest();
         //runDatabaseQuery();
     }
  
@@ -53,7 +53,8 @@ public class App {
     private static void runGetRequest() {
  
     	// sample URL
-        String url = "http://ip.jsontest.com/";
+        //String url = "http://ip.jsontest.com/";
+        String url = "http://140.86.15.104:3000/shield/33/45/red/EddyOnGitHub/";
         CloseableHttpResponse response = null;
  
         try {
